b0ac67ce4ff70a37bf8338ea3981b4a50b36094e
rafia9005 <rafia9005@gmail.com>
2024-12-30 21:40:47 +0700

    add latin translation

--- src/components/elements/VerseCard.tsx
+++ src/components/elements/VerseCard.tsx
@@ -9,6 +9,7 @@ const VerseCard: React.FC<VerseCardProps> = ({ verse }) => {
     <div className="max-w-xl bg-gray-800 border border-gray-700 rounded-lg shadow-md hover:shadow-lg transition-transform duration-300 transform mb-1">
       <div className="p-6">
         <p className="text-2xl font-semibold text-gray-100 mb-4">{verse.text.arab}</p>
+        <p className="text-gray-400 mb-5">{verse.text.transliteration.en}</p>
         <p className="text-gray-400">{verse.translation.id}</p>
         <audio controls src={verse.audio.primary} className="mt-4 w-full rounded-lg" />
       </div>
