1fca9f79b477f9b3417ac45a8bdda06ecd75dc1e
rafia9005 <rafia9005@gmail.com>
2024-12-30 20:11:54 +0700

    init react

--- src/utils/Fetch.ts
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-const API_URL = import.meta.env.API_URL;
-
-export const Fetch = axios.create({
-  baseURL: API_URL,
-  headers: {
-    "Content-Type": "application/json",
-  },
-});
